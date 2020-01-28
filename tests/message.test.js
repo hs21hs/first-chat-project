0532b0e2d5f276844f585955c829260ceeceff6f
github username <github email address>
2020-01-28 18:39:50 +0000

    used testing lifecycle method beforeEach

---
+++ tests/message.test.js
@@ -0,0 +1,10 @@
+// '/currentMessages' shld get all messages between 2 users
+//req.body.currentUser._id, req.body.chatPartner._id
+
+
+// '/messages' shld return 200 when passed with all attributes
+//     text:req.body.text,
+//     sender: req.body.sender,
+//     reciever: req.body.reciever
+
+// '/messages' shld return 400 when missing an attribute
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,8 +1,37 @@
 const request = require('supertest')
 const app = require('../src/app')
+const mongoose = require('mongoose')
+const User = require('../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+const userOne = {
+    _id: userOneId,
+    username: 'haider'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
 
 test('should sign up new user', async () => {
     await request(app).post('/users').send({
-        username: "newTtry13"
+        username: "newTtry135"
     }).expect(201)
 })
+
+test('should fail to sign up exsisting user', async () => {
+    await request(app).post('/users').send({
+        username: "haider"
+    }).expect(400)
+})
+
+//'/getAllUsers' shld get all users except the user id passed in req.body.currentUser._id
+// that array of users should also contain new msg true for one and false for another appropriately
+
+// '/users/login' shld respond 200 when existing user is passsed in req.body.currentUser._id
+// '/users/login' shld respond 400 when non-existing user is passsed in req.body.currentUser._id
+
+
+
+
