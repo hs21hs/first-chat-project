760e91f2f2795301014e98c8b28324b3a7555e25
github username <github email address>
2020-02-03 23:10:13 +0000

    correcting notifications for new messages

--- src/sockets/starting.js
+++ src/sockets/starting.js
@@ -8,38 +8,24 @@ const confirm = (socket) => {
 
 io.on('connection', (socket) => {
     console.log("connectioned")
-    recievedMessage(socket)
     newMatch(socket)
+    newMessage(socket)
 })
 
-const createMessage = (messageInfo, socket) => {
-    const message = new Message({
-        text: messageInfo.message,
-        sender: messageInfo.sender,
-        reciever: messageInfo.reciever
-    })
 
-    message.save().then((m) => {
-        
-        socket.emit('bc', m)
-    }).catch((e) => {
-        
-    })
-}
-
-const recievedMessage = (socket) => {
-    socket.on("newMessage", (messageInfo) => {
-        
-        createMessage(messageInfo, socket)
-    })
-}
-}
 
 const newMatch = (socket) => {
     socket.on(("newMatch"), (match) => {
         socket.broadcast.emit('newMatch', match)
         
     })
+}
+
+const newMessage = (socket) => {
+    socket.on("newMessage", (msg) => {
+        socket.broadcast.emit('newMessage', msg)
+    })
+}
 }
 
 
