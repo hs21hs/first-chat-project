efbe128e45f8040cddb57d12ccfa7873d4961b9d
github username <github email address>
2020-05-05 16:50:35 +0100

    changes for heroku

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-mongoose.connect('mongodb://127.0.0.1:27017/'+process.env.DBNAME,{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/'+process.env.DBNAME,{
     useUnifiedTopology: true,
     useCreateIndex: true,
     useNewUrlParser: true 
